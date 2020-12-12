0c0c9ce8299d7bde4b001c5ee5482a3764f01f52
Alexiz Hernandez <alexiz.developer@gmail.com>
2020-12-12 00:09:33 -0700

    added dotenv for environment variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { testVar } from '../settings';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => {
-  res.status(200).json({ message: 'MyBills API' });
+  res.status(200).json({ message: testVar });
 });
 
 export default indexRouter;
---
+++ src/settings.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const testVar = process.env.TEST_VAR;
