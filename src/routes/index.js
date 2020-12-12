7bafcb134f6cb200b101e1154b7b1165f3df8d3b
Alexiz Hernandez <alexiz.developer@gmail.com>
2020-12-12 00:06:06 -0700

    added babel, prettier and eslint and converted code to es6

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-var express = require("express");
+import express from 'express';
 
-var router = express.Router();
+const indexRouter = express.Router();
 
-router.get("/", function (req, res, next) {
-  return res.status(200).json({ message: "MyBills API" });
+indexRouter.get('/', (req, res) => {
+  res.status(200).json({ message: 'MyBills API' });
 });
 
-module.exports = router;
+export default indexRouter;
